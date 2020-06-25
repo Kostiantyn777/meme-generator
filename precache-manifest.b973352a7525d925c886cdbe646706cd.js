ee60bce8600d4fa4943d7887f5cf3224e01bb011
Kos <locoprodukten@gmail.com>
2020-06-25 14:49:29 +0200

    Updates

---
+++ precache-manifest.b973352a7525d925c886cdbe646706cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3e1231402c9d513d6c973708adea84",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "9c397e9daa84ca1cf572",
+    "url": "/meme-generator/static/css/main.fb5cc63e.chunk.css"
+  },
+  {
+    "revision": "8b0bb354c7f08c47e655",
+    "url": "/meme-generator/static/js/2.4bca04ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.4bca04ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c397e9daa84ca1cf572",
+    "url": "/meme-generator/static/js/main.e8448d5c.chunk.js"
+  },
+  {
+    "revision": "163fc7ea317d65653242",
+    "url": "/meme-generator/static/js/runtime-main.66d5941a.js"
+  }
+]);
